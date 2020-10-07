4f72d3fe30d560dd29bb3e9429792276bc5f19ff
NathanSilvaSantos <nathan013silva@gmail.com>
2020-10-07 02:41:02 -0300

    criando função para listar todos os produtos

--- GetProducts/index.js
+++ GetProducts/index.js
@@ -1,13 +1,13 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const createMongoClient = require('../shared/mongoClient');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+module.exports = async function (context, req) {
+    const { client: MongoClient, closeConnectionFn } = await createMongoClient();
+    const Products = MongoClient.collection('products');
+    const res = await Products.find({});
+    const body = await res.toArray();
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        status: 200,
+        body,
+    }
 }
